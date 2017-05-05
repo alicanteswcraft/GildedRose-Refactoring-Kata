72f0d8908c87e4e41eb0e8653405074c300595db
Ruben Anton <rubocoptero@gmail.com>
2017-05-05 19:41:06 +0200

    State at the end of the meetup

--- js/src/gilded_rose.js
+++ js/src/gilded_rose.js
@@ -6,57 +6,107 @@ class Item {
   }
 }
 
+const MAX_QUALITY = 50
+const DOUBLE_INCREMENT_THRESHOLD = 10
+const TRIPLE_INCREMENT_THRESHOLD = 5
+
 class Shop {
   constructor(items=[]){
     this.items = items;
   }
   updateQuality() {
-    for (var i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
+    this.items.forEach((item, i) => {
+      this.items[i] = this.updateItem(item)
+    });
+
+    return this.items;
+  }
+
+  isNotAgedBrie(item) {
+    return item.name != 'Aged Brie';
+  }
+
+  isAgedBrie(item) {
+    return item.name == 'Aged Brie';
+  }
+
+  isNotBackstagePasses(item) {
+    return ! this.isBackstagePasses(item);
+  }
+
+  isBackstagePasses(item) {
+    return item.name == 'Backstage passes to a TAFKAL80ETC concert';
+  }
+
+  increaseQuality(item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality = item.quality + 1
+    }
+  }
+
+  isNotSulfuras(item) {
+    return item.name != 'Sulfuras, Hand of Ragnaros'
+  }
+
+  hasQuality(item) {
+    return item.quality > 0
+  }
+
+  decreaseQuality(item) {
+    if (this.hasQuality(item) && this.isNotSulfuras(item)) {
+      item.quality = item.quality - 1
+    }
+  }
+
+  dropQuality(item) {
+    item.quality = 0
+  }
+
+  updateSellIn(item) {
+    if (this.isNotSulfuras(item)) {
+      item.sellIn = item.sellIn - 1;
+    }
+  }
+
+  increaseQuality(item) {
+    if (item.quality == MAX_QUALITY) {
+      return;
+    }
+    
+    item.quality = item.quality + 1
+  }
+
+  updateItem(item) {
+    if (this.isAgedBrie(item)) {
+      this.increaseQuality(item)
+    } else if (this.isBackstagePasses(item)) {
+      this.increaseQuality(item)
+      if (item.quality < MAX_QUALITY) {
+        if (this.isBackstagePasses(item)) {
+          if (item.sellIn <= DOUBLE_INCREMENT_THRESHOLD) {
+            this.increaseQuality(item)
+          }
+          if (item.sellIn <= TRIPLE_INCREMENT_THRESHOLD) {
+            this.increaseQuality(item)
           }
         }
+      }
+    } else {
+      this.decreaseQuality(item)
+    }
+    this.updateSellIn(item)
+    if (item.sellIn < 0) {
+      if (this.isAgedBrie(item)) {
+        this.increaseQuality(item)
       } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
+        if (this.isBackstagePasses(item)) {
+          this.dropQuality(item)
         } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
+          this.decreaseQuality(item)
         }
       }
     }
 
-    return this.items;
+    return item;
   }
 }
